3f7c68a1efb4bae00bea1269125b06a8534cfab2
Marc DeRosa <marc.derosa@gmail.com>
2010-04-30 21:03:07 +0800

    needed for lein

---
+++ pelrapeire/project.clj
@@ -0,0 +1,8 @@
+(defproject compjtest "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+		 [compojure/compojure "0.4.0-SNAPSHOT"]
+		 [ring/ring-jetty-adapter "0.2.M"]
+		 [hiccup/hiccup "0.2.3"]]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.2.0-SNAPSHOT"]])
