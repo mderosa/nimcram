154bb966707ff7eebe3c02f43d4b672513357edb
Marc DeRosa <marc.derosa@gmail.com>
2010-05-09 17:28:46 +0800

    drag and drop is working

--- pelrapeire/js/projects/n/home.js
+++ pelrapeire/js/projects/n/home.js
@@ -3,42 +3,62 @@ var y;
 YUI().use('dd-drop', 'dd-proxy', 'node-base', function(Y) {
 	y = Y;
 	
-    var bbTasks = Y.one('#backburner').all('.task');
-    Y.each(bbTasks, function(v, k) {
+    var tasks = Y.Node.all('div.tasks table.task');
+    Y.each(tasks, function(v, k) {
     	var drag = new Y.DD.Drag({
     		node: v,
-    		dragMode: 'intersect'
+    		target: {
+    			border: '0 0 0 20'
+    		}
     	}).plug(Y.Plugin.DDProxy, {moveOnEnd: false});
-    	
-    	drag.on('drag:start', function() {
-        	var p = this.get('dragNode'),
-        		n = this.get('node');
-        	n.setStyle('opacity', .25);
-        	p.set('innerHTML', n.get('innerHTML'));
-        	p.setStyles({opacity: .65});
-        });
-    	
-    	drag.on('drag:end', function() {
-    		var n = this.get('node');
-    		n.setStyle('opacity', 1);
-    	});
     });
 
-    var actTasks = Y.one('#active').all('.tasks');
-    Y.each(actTasks, function(v, k) {
+    var tasksDiv = Y.Node.all('table.task');
+    Y.each(tasksDiv, function(v, k) {
         var drop = new Y.DD.Drop({node: v});       
     });
-
-    function resizeTasks() {
-	var heightY = Y.DOM.winHeight();
-    	var ls = Y.all('div.tasks');
-    	ls.setStyle('height', (heightY - 175));
-    };
-    resizeTasks();
-
-    Y.on('resize', function(e){
-        resizeTasks();
+    var tasksHeader = Y.Node.all('div.bmrcp-head');
+    Y.each(tasksHeader, function(v, k) {
+    	var drop = new Y.DD.Drop({node: v});
     });
-    
+
+   function resizeTasks() {
+	   var heightY = Y.DOM.winHeight();
+	   var ls = Y.all('div.tasks');
+	   ls.setStyle('height', (heightY - 175));
+   };
+   resizeTasks();
+
+   Y.on('resize', function(e){
+	   resizeTasks();
+   });
+
+   Y.DD.DDM.on('drag:start', function(e) {
+	    var drag = e.target;
+	    drag.get('node').setStyle('opacity', '.25');
+	    drag.get('dragNode').set('innerHTML', drag.get('node').get('innerHTML'));
+	    drag.get('dragNode').setStyles({opacity: '.65'});
+	});
+   
+   Y.DD.DDM.on('drag:end', function(e) {
+	    var drag = e.target;
+	    drag.get('node').setStyles({opacity: '1'});
+	});
+   
+   Y.DD.DDM.on('drag:drophit', function(e) {
+	   var ndDrag = e.drag.get('node');
+	   var ndDrop = e.drop.get('node');
+	   if (ndDrop.get('tagName').toLowerCase() === 'table') {
+		   ndDropParent = ndDrop.get('parentNode');
+		   ndDrop = ndDrop.get('nextSibling');
+		   if (ndDrop) {
+			   ndDropParent.insertBefore(ndDrag, ndDrop);
+		   } else {
+			   ndDropParent.appendChild(ndDrag);
+		   }
+	   } else if (ndDrop.get('tagName').toLowerCase() === 'div' && ndDrop.hasClass('bmrcp-head')) {
+		   ndDrop.get('nextSibling').prepend(ndDrag);
+	   }
+   });
 
 });
--- pelrapeire/src/pelrapeire/pages/projects/n/home.clj
+++ pelrapeire/src/pelrapeire/pages/projects/n/home.clj
@@ -30,7 +30,6 @@
       [:span
        [:a {:href "#" :style "margin-left:20px"} "[new]"]]]
      [:div {:class "tasks"}
-      [:table {:class "task"} [:tr [:td]]]
       (make-task :north-east "another task that has a really really really long title that takes up a lot of space and may push stuff out of the way" 0)
       (make-task :north-east "some task here" 0)
       (make-task :north-east "some task here2" 0)
@@ -40,11 +39,13 @@
       [:div {:class "bmrcp-e"}
        [:div {:class "bmrcp-w"}]]]
      [:div {:class "bmrcp-head"} "work in progress"]
-     [:div {:class "tasks"}]]
+     [:div {:class "tasks"}
+]]
     [:td {:id "live" :class "bucket"}
      [:div {:class "bmrcp-n"}
       [:div {:class "bmrcp-e"}
        [:div {:class "bmrcp-w"}]]]
      [:div {:class "bmrcp-head"} "delivered to user"]
-     [:div {:class "tasks"}]]]])
+     [:div {:class "tasks"}
+]]]])
      
