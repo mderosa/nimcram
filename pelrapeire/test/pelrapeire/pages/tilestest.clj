c3c09a833b603ad79e45c243e0d02c0c3a80f9bd
Marc DeRosa <marc.derosa@gmail.com>
2010-05-23 15:39:45 +0800

    test

---
+++ pelrapeire/test/pelrapeire/pages/tilestest.clj
@@ -0,0 +1,16 @@
+
+(ns pelrapeire.pages.tilestest
+  (:use clojure.test
+	hiccup.core
+	pelrapeire.pages.tiles
+	pelrapeire.pages.pagedefinition))
+
+(deftest tile-structure-test 
+  (let [output (project-tile project-home)]
+	(is (.contains output (project-home :js)))
+	(is (.contains output (project-home :title)))
+	(is (.contains output "table"))))
+
+
+
+
