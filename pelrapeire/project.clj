8d2d786b44a63e0d59fe3722e0ca306857878d14
Marc DeRosa <marc.derosa@gmail.com>
2010-06-10 22:30:28 +0800

    add http client libs

--- pelrapeire/project.clj
+++ pelrapeire/project.clj
@@ -4,5 +4,7 @@
                  [org.clojure/clojure-contrib "1.1.0"]
 		 [compojure/compojure "0.4.0-SNAPSHOT"]
 		 [ring/ring-jetty-adapter "0.2.M"]
-		 [hiccup/hiccup "0.2.3"]]
+		 [hiccup/hiccup "0.2.3"]
+		 [org.apache.httpcomponents/httpcore "4.0.1"]
+		 [org.apache.httpcomponents/httpclient "4.0.1"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.0-SNAPSHOT"]])
