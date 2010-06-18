06c35ee30def547a4c34669e11df286a1ebd088b
Marc DeRosa <marc.derosa@gmail.com>
2010-06-18 22:10:01 +0800

    getting closer on db access code

--- pelrapeire/src/pelrapeire/repository/tasks.clj
+++ pelrapeire/src/pelrapeire/repository/tasks.clj
@@ -1,34 +1,35 @@
 
 (ns pelrapeire.repository.tasks
-  (:use pelrapeire.repository.uuid)
+  (:use pelrapeire.repository.uuid
+	clojure.contrib.json.read
+	clojure.contrib.json.write)
   (:import org.apache.http.HttpEntity
 	   (org.apache.http.client HttpClient ResponseHandler)
-	   (org.apache.http.client.methods HttpGet HttpPut)
+	   (org.apache.http.client.methods HttpGet HttpPut HttpDelete)
 	   org.apache.http.entity.StringEntity
 	   (org.apache.http.impl.client BasicResponseHandler DefaultHttpClient)
 	   org.apache.http.client.methods.HttpGet))
 
-;(defn 
-;  #^{:tag String
-;     :db-resp-error "{'error':'not_found','reason':'missing'}"
-;     :doc "Gets an existing document from the database"
-;     }
-;  get-task [map cfg]
-;    (let [http-client (DefaultHttpClient.)
-;	  get (doto (HttpGet. (str (:url cfg) "338e33077b1c67b3bddbaf38e8252c2b"))
-;		(.addHeader "Accept" "application/json"))
-;	  rsp-handler (BasicResponseHandler.)]
-;    (. http-client execute get rsp-handler)))
+(defn 
+  #^{:tag String
+     :doc "Gets an existing document from the database"
+     :throws "throws an HttpResponseException for any https response code >= 400"
+     }
+  get-task [#^String id cfg]
+    (let [http-client (DefaultHttpClient.)
+	  get (doto (HttpGet. (str (:url cfg) id))
+		(.addHeader "Accept" "application/json"))
+	  rsp-handler (BasicResponseHandler.)]
+    (. http-client execute get rsp-handler)))
 
 (defn 
   #^{:tag String
      :db-resp-ok "{'ok':true,'id':'11','rev:'1-55fa92c1a80a923f12ca22e79eefded1'}"
-     :db-resp-error "{'error':'conflict','reason':'Document update conflict.'}"
      :doc "create-task :: String -> Cfg -> Either JsonOk JsonError. Note this
 function does not do any translation of json strings to clojure maps"
      :throws "throws an HttpResponseException for response codes >= 400"
      }
-  create-task [json cfg]
+  create-task [#^String json cfg]
   (let [http-client (DefaultHttpClient.)
 	entity (StringEntity. json)
 	put (doto (HttpPut. (str (:url cfg) (uuid)))
@@ -38,20 +39,32 @@ function does not do any translation of json strings to clojure maps"
 	rsp-handler (BasicResponseHandler.)]
     (. http-client execute put rsp-handler)))
 
-;(defn 
-;  #^{:tag String
-;     :db-resp-error "{'error':'bad_content_type','reason':'Invalid Content-Type header for form upload'}"
-;     :doc "This function makes a put request that overwrites an existing record.  For the overwrite to
-;be successful the request json must contain a '_rev' parameter that matches the current revision number
-;of the document
-;The parameters are a map containing a partail or complete task and a mode which is either :overwrite or 
-;:append"
-;     }
-;  update-task [map cfg mode]
-;  )
+(defn 
+  #^{:doc "for the delete request we need the the task number in the url and also the revision 
+number as a parameter."}
+  delete-task [id rev cfg]
+  (let [http-client (DefaultHttpClient.)
+	del (doto (HttpDelete. (str (:url cfg) id "?rev=" rev))
+	      (.addHeader "Accept" "application/json"))
+	rsp-handler (BasicResponseHandler.)]
+    (. http-client execute del rsp-handler)))
 
-;(defn 
-;  #^{:doc "all we need for the delete request is the task number, no revision 
-;number is needed."}
-;  delete-task [map cfg]
-;  )
+(defn 
+  #^{:tag String
+     :doc "This function makes a put request that overwrites an existing record.  For the overwrite to
+be successful the request json must contain a '_rev' parameter that matches the current revision number
+of the document
+The parameters are a map containing a partail or complete task and a mode which is either :overwrite or 
+:append"
+     }
+  update-task [#^String json cfg mode]
+  {:pre [(and (. json contains "id") (. json contains "rev"))
+	 (or (= mode :append) (= mode :write))
+	 (not (nil? (:url cfg)))]}
+  (if (= mode :append)
+    (let [json-upd (read-json-string json)
+	  json-cur (read-json-string (get-task (json-upd "id") cfg))
+	  req (assoc json-cur json-upd)]
+      (create-task (json-str req) cfg))
+    (create-task json cfg)))
+
--- pelrapeire/test/pelrapeire/repository/taskstest.clj
+++ pelrapeire/test/pelrapeire/repository/taskstest.clj
@@ -1,15 +1,51 @@
 
 (ns pelrapeire.repository.taskstest
   (:use clojure.test
+	clojure.contrib.trace
 	pelrapeire.dbconfig
-	pelrapeire.repository.tasks))
+	pelrapeire.repository.tasks)
+  (:import org.apache.http.client.HttpResponseException))
 
-(deftest test-create-task 
-  (let [rst (create-task "{\"name\": \"marc\", \"age\": 2}" db-config-int)]
-    (is (. rst contains "\"ok\":true"))
-    (is (. rst contains "\"rev\""))))
+(deftest test-get-task-doesnt-exist
+  (testing "should throw exception when a doc doesnt exist"
+    (is (thrown? HttpResponseException (get-task "doesntExist" db-config-int)))))
 
-(deftest test-equals
-  (let [one 1
-	two 2]
-    (is (+ one 1) two)))
+(deftest test-get-task-exists
+  (testing "should find the test doc id = 100"
+  (let [rsp (get-task "100" db-config-int)]
+    (is (. rsp contains "a document for testing")))))
+
+(deftest test-delete-not-exist-task
+  (is (thrown? HttpResponseException (delete-task "doesntExist" "23" db-config-int))))
+
+(defn extract-id [s] 
+  (let [regex-id #"\"id\":\"([a-zA-Z0-9]+)\""]
+    (get (re-find regex-id s) 1)))
+(defn extract-rev [s] 
+  (let [regex-id #"\"rev\":\"([a-zA-Z0-9-]+)\""]
+    (get (re-find regex-id s) 1)))
+
+(deftest test-extract-id 
+  (is (= "1abZdi3" (extract-id "{\"id\":\"1abZdi3\", \"actual\":true}"))))
+(deftest test-extract-rev
+  (is (= "2-2f80c392bf0d5f4897178567f56689c9" (extract-rev "{\"ok\":true,\"id\":\"017d482a95684ce799f7d3309e058d62\",\"rev\":\"2-2f80c392bf0d5f4897178567f56689c9\"}"))))
+
+(defn add-then-delete []
+  (let [rsp (create-task "{\"name\":\"a record to delete\"}" db-config-int)
+	id (extract-id rsp)
+	rev (extract-rev rsp)]
+    (delete-task id rev db-config-int)))
+
+(deftest test-add-then-delete
+  (is (. (add-then-delete) contains "ok")))
+
+(deftest update
+  (testing "obj'(rev=n-1) update(:append | write) obj(rev=n) -> exception"
+    (is (true? false)))
+  (testing "obj'(rev=n) update(:append) obj(rev=n) -> merged obj"
+    (is (true? false)))
+  (testing "obj'(rev=n) update(:write) obj(rev=n) -> obj'(rev=n+1)"
+    (is (true? false))))
+
+    
+
