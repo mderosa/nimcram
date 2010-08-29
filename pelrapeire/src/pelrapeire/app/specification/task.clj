30b1d7b6a96abac3aa71f06a9cb636ec570b202c
Marc DeRosa <marc.derosa@gmail.com>
2010-08-29 21:55:25 +0800

    namespace storage bug fix

--- pelrapeire/src/pelrapeire/app/specification/task.clj
+++ pelrapeire/src/pelrapeire/app/specification/task.clj
@@ -3,6 +3,22 @@
   (:require [clojure.contrib.str-utils2 :as s])
   (:import org.joda.time.DateTime))
 
+(defn ns-string-to-map [ns]
+  (if (s/blank? ns) 
+    nil
+    (let [halves (. ns split "=")]
+      (cond 
+       (not (= 2 (alength halves))) nil
+       (s/blank? (aget halves 0)) nil
+       (s/blank? (aget halves 1)) nil
+       true {(aget halves 0) (aget halves 1)}))))
+
+(defn ns-param-to-vector-map [ns]
+    (cond 
+     (nil? ns) []
+     (string? ns) (ns-param-to-vector-map [ns])
+     (vector? ns) (filter #(not (nil? %)) (map ns-string-to-map ns))))
+
 (defn 
   #^{:doc "this function is responsible for creating a task that meets
 specifications"}
@@ -18,8 +34,10 @@ specifications"}
 		    true
 		    false)
 	create-dt (datetime-to-vector (DateTime.))
+	nm-space (ns-param-to-vector-map (map-data "namespace"))
 	conditioned-data (assoc map-data "specification" spec
 				"deliversUserFunctionality" user-func
+				"namespace" nm-space
 				"taskCreateDate" create-dt)
 
 	default {"type" "task" 
@@ -33,5 +51,5 @@ specifications"}
 		 "taskCompleteDate" nil
 		 "progress" "proposed"
 		 "priority" nil
-		 "namespace" []}]
+		 "namespace" nm-space}]
     (conj default conditioned-data)))
