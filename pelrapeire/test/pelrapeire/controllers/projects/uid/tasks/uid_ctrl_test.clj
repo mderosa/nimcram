8924f1e8ed6b7e9c0dcd9f85bf47d9243013c718
Marc DeRosa <marc.derosa@gmail.com>
2010-09-26 22:14:32 +0800

    backend tasks for update progress

--- pelrapeire/test/pelrapeire/controllers/projects/uid/tasks/uid_ctrl_test.clj
+++ pelrapeire/test/pelrapeire/controllers/projects/uid/tasks/uid_ctrl_test.clj
@@ -6,6 +6,7 @@
   (testing "that when we are sending _id _rev progress and taskStartDate params to the db"
     (let [actual (run-update-progress (fn [x y] x) {"_id" "688e77149276420e36d6206d142e71a2" 
 						    "_rev" "6-0653ce615f141ebff9cccca7fd5df1db" 
+						    "originalProgress" "proposed"
 						    "progress" "in-progress"})]
       (is (actual "_id"))
       (is (actual "_rev"))
@@ -35,3 +36,36 @@
 	  (is (nil? (actual "task-uid")))
 	  (is (actual "deliversUserFunctionality")))))
 
+(deftest test-update-all-progress
+  (testing "test that one can not update progress unless one adds an action parameter"
+    (let [actual (run-update-all (fn [x y] x) {"_id" "96f8f20ae415eab09b962332c188a8c0"
+					     "_rev" "2-77d0f3aca83127bfc2b7a76d7858dff4"
+					     "project-uid" "PicoMinMin"
+					     "task-uid" "96f8f20ae415eab09b962332c188a8c0"
+					     "progress" "delivered"
+					     "title" "a title"
+					     "deliversUserFunctionality" "true"
+					     "specification" "spec"})]
+      (is (nil? (actual "progress"))))))
+
+(deftest test-run-update-progress-proposed-to-delivered
+  (testing "we should be able to move tasks directly from proposed to delivered"
+    (let [actual (run-update-progress (fn [x y] x) {"_id" "96f8f20ae415eab09b962332c188a8c0"
+						    "_rev" "2-77d0f3aca83127bfc2b7a76d7858dff4"
+						    "project-uid" "PicoMinMin"
+						    "task-uid" "96f8f20ae415eab09b962332c188a8c0"
+						    "originalProgress" "proposed"
+						    "progress" "delivered"})]
+      (is (= "delivered" (actual "progress")))
+      (is (not (nil? (actual "taskStartDate"))))
+      (is (not (nil? (actual "taskCompleteDate")))))))
+
+(deftest test-run-update-progress-delivered-to-proposed
+  (testing "we should be able to move stuff from delivered to proposed but I wont reset the start date"
+    (let [actual (run-update-progress (fn [x y] x) {"_id" "96f8f20ae415eab09b962332c188a8c0"
+						    "_rev" "2-77d0f3aca83127bfc2b7a76d7858dff4"
+						    "project-uid" "PicoMinMin"
+						    "task-uid" "96f8f20ae415eab09b962332c188a8c0"
+						    "originalProgress" "delivered"
+						    "progress" "proposed"})]
+      (is (nil? actual)))))
