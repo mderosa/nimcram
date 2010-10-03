92706c023b39b5c4fedcac714ec6b56bf74afa73
Marc DeRosa <marc.derosa@gmail.com>
2010-10-03 09:06:12 +0800

    update the project file to point to all the correct latest jars

--- pelrapeire/project.clj
+++ pelrapeire/project.clj
@@ -1,12 +1,12 @@
 (defproject compjtest "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-		 [compojure/compojure "0.4.0-SNAPSHOT"]
-		 [ring/ring-jetty-adapter "0.2.M"]
-		 [ring/ring-devel "0.2"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha1"]
+                 [org/clojure/contrib/complete "1.3.0-alpha1"]
+		 [compojure/compojure "0.5.1"]
+		 [ring/ring-jetty-adapter "0.3.0"]
+		 [ring/ring-devel "0.3.0"]
 		 [hiccup/hiccup "0.2.3"]
 		 [org.apache.httpcomponents/httpcore "4.0.1"]
 		 [org.apache.httpcomponents/httpclient "4.0.1"]
 		 [joda-time/joda-time "1.6"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure/swank-clojure "1.3.0-SNAPSHOT"]])
