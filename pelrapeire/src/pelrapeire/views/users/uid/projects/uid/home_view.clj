4e69d46919ede999cd6fc83ed0b169546f7c9feb
Marc DeRosa <marc.derosa@gmail.com>
2010-11-25 14:28:54 +0800

    we have graphics linked in

--- pelrapeire/src/pelrapeire/views/users/uid/projects/uid/home_view.clj
+++ pelrapeire/src/pelrapeire/views/users/uid/projects/uid/home_view.clj
@@ -10,12 +10,15 @@
 	xbar-data {:xbarbar (average xs)
 		   :xbarucl (xbar-ucl xs ss 3)
 		   :xbarlcl (xbar-lcl xs ss 3)
-		   :subgroup-avgs xs
+		   :subgroupavgs xs
 		   }]
-  {:js nil :css nil :title "your home" :content 
+  {:js ["/js/protovis/protovis-d3.2.js"] :css nil :title "your home" :content 
    [:div 
     [:div {:id "xbar"}
-     [:script (str "var xbardata=" (json-str xbar-data))]]
+     [:script (str "var xbardata=" (json-str xbar-data) ";")]
+     [:script {:src "/js/users/uid/projects/uid/home.js" :type "text/javascript"}]
+     ]
+    
     [:div {:id "xbardescription"}
      [:p (str "average work in-progress time (hours): " (double (/ (Math/round (double (* 10 (:xbarbar xbar-data)))) 10)))]
      [:p (str "average work in-progress time (days): " (double (/ (Math/round (* 10 (double (/ (:xbarbar xbar-data) 24)))) 10)))]
