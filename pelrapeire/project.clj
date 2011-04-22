49afd174065676974be5ff717324991b67485563
Marco <marc.derosa@gmail.com>
2011-04-22 19:26:35 +0800

    fix project file so we can download jars without errors

--- pelrapeire/project.clj
+++ pelrapeire/project.clj
@@ -1,7 +1,7 @@
 (defproject hokulea "1.0"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.3.0-alpha1"]
-                 [org/clojure/contrib/complete "1.3.0-alpha1"]
+                 [org/clojure/contrib/complete "1.3.0-alpha1" :classifier "bin"]
 		 [compojure/compojure "0.5.1"]
 		 [ring/ring-core "0.3.0"]
 		 [ring/ring-servlet "0.3.0"]
@@ -14,7 +14,7 @@
 		 [joda-time/joda-time "1.6"]
 		 [javax.mail/mail "1.4.1"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.3.0-SNAPSHOT"]]
-  :jar-dir "target/"
+  :target-dir "target/"
   :test-selectors {:default (fn [v] (not (:integration v)))
 		   :integration :integration
 		   :all (fn [_] true)})
